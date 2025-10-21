80f70593b4bef04186dceeae5904c9e0622fdcbe
bidermanjulia8-source <bidermanjulia8@gmail.com>
2025-10-20 21:33:50 -0700

    Add files via upload

---
+++ Coding_Journal1.py
@@ -0,0 +1,9 @@
+full_name = "Julia Biderman"
+pronouns = "she/her"
+favorite_movie = "US and The Hangover"
+favorite_food = "Garlic Mashed Potatoes"
+
+print("Full Name:", full_name)
+print("Preferred Pronouns:", pronouns)
+print("Some of my favorite movies are", favorite_movie + ". I Love any Jordan Peele horror movie and a good comedy.")
+print("My favorite food is", favorite_food + ". I'll always eat mashed potatoes, with anything, at anytime!")
