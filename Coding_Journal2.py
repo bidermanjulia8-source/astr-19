c63d0ae0663dccda7003c9c225727d78eed4ce74
bidermanjulia8-source <bidermanjulia8@gmail.com>
2025-10-20 21:55:03 -0700

    Add files via upload

---
+++ Coding_Journal2.py
@@ -0,0 +1,19 @@
+# Sum of two floating point numbers 
+float_1 = 3.9
+float_2= 2.8
+sum_result = float_1 + float_2
+print("Data type:", type(sum_result))
+print("Sum of two floating point numbers:", sum_result)
+# Difference between two integers
+int_1 = 8924
+int_2 = 8857
+difference_result = int_1 - int_2
+print("Data type:", type(difference_result))
+print("Difference between two integers:", difference_result)
+
+#Product of a floating point number and an integer
+float1 = 1
+int2 = 67
+product_result = float1 * int2
+print("Data type:", type(product_result))
+print("Product of a float and an integer:", product_result)
